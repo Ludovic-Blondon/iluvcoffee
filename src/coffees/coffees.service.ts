19ada87d080771e866fb96194ebc4724464c2d49
lblondon <ludovicblondon@gmail.com>
2025-07-06 17:56:26 +0200

    feat: add event tracking for coffee recommendations
    - Introduce Event entity to log coffee recommendation events
    - Update Coffee entity to include recommendations count
    - Enhance CoffeesService with recommendCoffee method to handle recommendations and event logging
    - Modify CoffeesModule to include Event entity in TypeORM configuration

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entities';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Event } from 'src/events/entities/event.entity';
 
 @Injectable()
 export class CoffeesService {
@@ -15,6 +16,7 @@ export class CoffeesService {
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
+        private readonly dataSource: DataSource,
     ) { }
 
     findAll(paginationQuery: PaginationQueryDto) {
@@ -81,5 +83,24 @@ export class CoffeesService {
             return existingFlavor;
         }
         return this.flavorRepository.create({ name: flavorName });
+    }
+
+    async recommendCoffee(coffee: Coffee) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            coffee.recommendations++;
+            const recommendEvent = new Event();
+            recommendEvent.name = 'recommend_coffee';
+            recommendEvent.type = 'coffee';
+            recommendEvent.payload = { coffeeId: coffee.id };
+            await queryRunner.manager.save(Event, recommendEvent);
+            await queryRunner.commitTransaction();
+        } catch (error) {
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+        }
     }
 }
