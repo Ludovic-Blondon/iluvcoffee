fdcf825c20878046ed23ff07c458c6ce46636ca8
lblondon <ludovicblondon@gmail.com>
2025-07-06 17:15:42 +0200

    feat: add Flavor entity and establish many-to-many relationship with Coffee
    - Introduce Flavor entity to represent coffee flavors
    - Update Coffee entity to include a many-to-many relationship with Flavor
    - Modify CoffeesService to handle flavor associations in CRUD operations
    - Enhance findAll and findOne methods to include related flavors

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,9 +3,10 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entities';
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Coffee])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
     controllers: [CoffeesController],
     providers: [CoffeesService],
 })
