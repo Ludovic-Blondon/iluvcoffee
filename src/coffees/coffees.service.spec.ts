9f53b68d32a8d1d46058f4e45d8110055052d6d6
Ludovic BLONDON <43740382+Ludovic-Blondon@users.noreply.github.com>
2025-07-06 21:24:13 +0200

    Fix/test (#2)
    * fix: update greeting message in AppController test from "Hello World!" to "Hello Nest!"
    
    * test: enhance CoffeesController and CoffeesService tests with mock dependencies

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { DataSource } from 'typeorm';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: {}, // 👈 on mock le repository
+        },
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: {}, // 👈 mock aussi
+        },
+        {
+          provide: DataSource,
+          useValue: {}, // 👈 et encore un mock
+        },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
