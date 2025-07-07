1539cb99583c3c733f0e168d176df80b7f9b4284
lblondon <ludovicblondon@gmail.com>
2025-07-07 21:31:48 +0200

    feat: add environment configuration and update dependencies
    - Introduce .env.example file for database configuration
    - Add @nestjs/config package to manage environment variables
    - Update TypeOrmModule configuration in AppModule to use environment variables for PostgreSQL connection
    - Update package-lock.json and package.json to include new dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT! || 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
