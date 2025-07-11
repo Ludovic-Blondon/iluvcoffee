57a0e539e94b7a137d2e81f13a99b763c448ddea
lblondon <ludovicblondon@gmail.com>
2025-07-11 20:02:44 +0200

    feat: integrate Swagger for API documentation
    - Add @nestjs/swagger and swagger-ui-express dependencies to package.json and package-lock.json
    - Update nest-cli.json to include Swagger plugin
    - Configure Swagger in main.ts to generate API documentation for the application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(
     new TimeoutInterceptor(),
   );
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
