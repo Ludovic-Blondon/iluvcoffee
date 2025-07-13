fa87c3867b0b484adc6bd3441b30fdfe8a200d4e
lblondon <ludovicblondon@gmail.com>
2025-07-13 18:21:23 +0200

    chore: update docker-compose configuration and enhance test commands
    - Add a new test database service to docker-compose for isolated testing.
    - Modify existing docker commands in package.json to target specific services.
    - Update greeting message in AppService and corresponding e2e tests for consistency.
    - Introduce new e2e tests for the Coffees feature with initial test structure.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,7 +7,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -20,6 +20,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello Nest!');
+      .expect('Hello World!');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
---
+++ test/coffees/coffees.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { INestApplication } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { App } from 'supertest/types';
+
+describe('[Feature] Coffees - /coffees', () => {
+    let app: INestApplication<App>;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [
+                CoffeesModule,
+                TypeOrmModule.forRoot({
+                    type: 'postgres',
+                    host: 'localhost',
+                    port: 5433,
+                    username: 'postgres',
+                    password: 'pass123',
+                    database: 'postgres',
+                    autoLoadEntities: true,
+                    synchronize: true,
+                }),
+            ],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it.todo('Create [POST /]');
+    it.todo('Get all [GET /]');
+    it.todo('Get one [GET /:id]');
+    it.todo('Update one [PATCH /:id]');
+    it.todo('Delete one [DELETE /:id]');
+
+    afterAll(async () => {
+        if (app) {
+            await app.close();
+        }
+    });
+});
