05bd7614cfb5dfed49a60db4facf372160b98bb4
lblondon <ludovicblondon@gmail.com>
2025-07-14 09:59:55 +0200

    feat: integrate MongoDB with NestJS application
    - Add @nestjs/mongoose and mongoose to package.json for MongoDB support
    - Configure MongooseModule in AppModule for database connection
    - Update CoffeesModule to use Mongoose for data management
    - Refactor CoffeesService to perform CRUD operations with MongoDB
    - Define Coffee schema using Mongoose decorators in coffee.entities.ts

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoffeeSchema } from './entities/coffee.entities';
+import { Coffee } from './entities/coffee.entities';
 
 @Module({
+    imports: [MongooseModule.forFeature([{ name: Coffee.name, schema: CoffeeSchema }])],
     controllers: [CoffeesController],
     providers: [CoffeesService],
 })
